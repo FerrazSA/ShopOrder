82d4a23a9ecc004059ea03005830502c2c17e877
Felipe Ferraz de Santana <felipeferrazs@outlook.com>
2025-01-28 10:06:08 -0300

    Initital Commit

---
+++ src/test/java/com/dexdev/ShopOrder/ShopOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dexdev.ShopOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
