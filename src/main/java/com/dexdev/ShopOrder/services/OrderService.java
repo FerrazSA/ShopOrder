1a755e34646d7e6307ec541a809e73911b19d386
Felipe Ferraz de Santana <felipeferrazs@outlook.com>
2025-01-28 10:14:32 -0300

    Creating methods Total and Shipment

--- src/main/java/com/dexdev/ShopOrder/services/OrderService.java
+++ src/main/java/com/dexdev/ShopOrder/services/OrderService.java
@@ -1,7 +1,15 @@
 package com.dexdev.ShopOrder.services;
 
+import com.dexdev.ShopOrder.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+    @Autowired
+    private ShippingService shippingService;
+
+    public double Total(Order order) {
+        return (order.getPrice() - order.getDiscount()) + shippingService.Shipment(order);
+    }
 }
--- src/main/java/com/dexdev/ShopOrder/services/ShippingService.java
+++ src/main/java/com/dexdev/ShopOrder/services/ShippingService.java
@@ -1,7 +1,18 @@
 package com.dexdev.ShopOrder.services;
 
+import com.dexdev.ShopOrder.entities.Order;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShippingService {
+
+    public double Shipment(Order order) {
+        if (order.getPrice() < 100){
+            return 20;
+        } else if (order.getPrice() >= 100 && order.getPrice() < 200){
+            return 12;
+        } else {
+            return 0;
+        }
+    }
 }
